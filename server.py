e3ad076a732e74e4b6ab2422eb9c39a2bbf445f6
“abbymccollam3” <amccollam3@gatech.edu>
2024-10-29 10:50:19 -0700

    Addred server changes

--- server.py
+++ server.py
@@ -12,3 +12,47 @@ LOG MESSAGE FORMAT:
     User-Agent: curl/7.85.0
     Accept: */* 
 '''
+
+# run python -m http.server 8000
+# navigate http://localhost:8000 -> displays files/directories on port
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer 
+# HTTPServer (server_address, request handler class)
+
+PORT = 8000
+
+class Handler (SimpleHTTPRequestHandler): # inheriting Simple... (request, client_address, server, directory=None)
+    # handle GET requests
+    def do_GET(self):
+        # get client address
+        address = self.client_address[0]
+        
+        # get command, path, request_version
+        command = f"{self.command} {self.path} {self.request_version}"
+
+        host = self.headers.get('Host')
+        user_agent = self.headers.get('User-Agent')
+        accept = self.headers.get('Accept')
+
+        # log message
+        print(f"Received request from {address}") # (host, port)
+        print(command)
+        print(f"Host: {host}")
+        print(f"User-Agent: {user_agent}")
+        print(f"Accept: {accept}")
+
+        if self.path == '/hello':
+            # send response -> 200 = OK
+            self.send_response(200, "Hi")
+
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Hello, World!")
+
+        else:
+            super().do_GET()
+
+# Server setup
+with HTTPServer(('localhost', PORT), Handler) as server:
+    print(f"Server started at http://localhost:{PORT}")
+    server.serve_forever()
