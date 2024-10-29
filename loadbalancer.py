139409ee208dd0992d9eb65179f817cc30fc1274
abbymccollam3 <abby.mccollam@gmail.com>
2024-10-29 09:50:30 -0700

    Adding objectives

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,1 +1,10 @@
-print("hello abby!")
+# Author: Abby McCollam
+# Title: Load Balancer
+# Objectives: 
+#   Build a load balancer that can send traffic to two or more servers
+#   Health check the servers.
+#   Handle a server going offline (failing a health check).
+#   Handle a server coming back online (passing a health check).    
+
+print ("Hi")
+
