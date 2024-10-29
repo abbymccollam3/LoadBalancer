67a7146555f7d762dba084bb81864985037f55e3
abbymccollam3 <abby.mccollam@gmail.com>
2024-10-29 09:58:22 -0700

    message logging to confirm connecting

--- loadbalancer.py
+++ loadbalancer.py
@@ -8,3 +8,4 @@
 
 print ("Hi")
 
+print ("Abby")
---
+++ server.py
@@ -0,0 +1,14 @@
+# Author: Abby McCollam
+# Title: Server
+# Objectives: 
+#   start-up then listen for incoming connections and forward to server
+#   provide log message to verify incoming connection
+
+''' 
+LOG MESSAGE FORMAT:
+    Received request from 127.0.0.1
+    GET / HTTP/1.1
+    Host: localhost
+    User-Agent: curl/7.85.0
+    Accept: */* 
+'''
